b4c524e4b3ca12ee1691000e94f3cabaedb6c058
ummataliyev <djohnummataliyev@gmail.com>
2023-05-08 19:38:40 +0500

    Link for books page added

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from books.views import BooksView
 from books.views import BookDetailView
 
-
+app_name = "books"
 urlpatterns = [
     path("", BooksView.as_view(), name="list"),
     path("<int:id>/", BookDetailView.as_view(), name="detail"),
