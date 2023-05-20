b680c0b90acff8b3869ed84afe6502a22427355d
ummataliyev <djohnummataliyev@gmail.com>
2023-05-20 16:58:35 +0500

    Crispy Forms Added

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -26,11 +26,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     # local apps
     'books',
     'users',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/forms.py
+++ users/forms.py
@@ -22,3 +22,7 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'profile_picture') # noqa
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].widget.attrs.update({'class': 'form-control'}) # NOQA
